57d452e85fb97c804b2261e41ff29760b1584130
Denis Wolter <wolter.important@gmail.com>
2022-02-26 23:45:56 +0300

    Add AboutPage

--- src/features/AboutPage/AboutPage.js
+++ src/features/AboutPage/AboutPage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import AboutTop from "./components/AboutTop";
+import AboutBottom from "./components/AboutBottom";
 
 const AboutPage = () => {
   return (
     <>
       <Header/>
+        <AboutTop />
+        <AboutBottom />
       <Footer/>
     </>
   )
