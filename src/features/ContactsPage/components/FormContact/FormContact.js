b530bf0d9dec412306ec73320c6d245ccaff5a6a
Denis Wolter <wolter.important@gmail.com>
2022-03-04 10:51:13 +0300

    add form error handle

--- src/features/ContactsPage/components/FormContact/FormContact.js
+++ src/features/ContactsPage/components/FormContact/FormContact.js
@@ -1,74 +1,83 @@
-import React from "react";
+import React, {useState} from "react";
 import './FormContact.css'
 import {Field, Form, Formik} from "formik";
 import {ref, push, database} from "../../../../util/firebase";
+import PopUp from "../../../../components/PopUp";
 
 const FormContact = () => {
+  const [isVisibleError, setVisibleError] = useState(false);
+  const [ErrorText, setErrorText] = useState("");
 
   function sendForm(values) {
     if (values.email !== "" && values.message !== "") {
       push(ref(database, 'messages/'), values)
+    } else {
+      setErrorText("Заполните все обязательные поля");
+      setVisibleError(true);
     }
-    else console.log('Заполните все обязательные поля');
   }
 
   return (
-    <div className="formContact">
-      <p className="formContact__title">Свяжитесь с нами</p>
-      <div>
-        <Formik
-          initialValues={{
-            firstName: '',
-            secondName: '',
-            email: '',
-            message: '',
-          }}
-          onSubmit={async (values) => {
-            await new Promise((r) => setTimeout(r, 400));
-            sendForm(values);
-          }}
-        >
-          <Form className="FormContact__inside">
-            <label className="FormikContact__label" htmlFor="firstName">Имя</label>
-            <Field
-              className="FormikContact__input"
-              id="firstName"
-              name="firstName"
-              type="firstName"
-            />
+    <>
+      {isVisibleError && <PopUp callClose={setVisibleError} message={ErrorText}/>}
 
-            <label className="FormikContact__label" htmlFor="secondName">Фамилия</label>
-            <Field
-              className="FormikContact__input"
-              id="secondName"
-              name="secondName"
-              type="secondName"
-            />
+      <div className="formContact">
+        <p className="formContact__title">Свяжитесь с нами</p>
+        <div>
+          <Formik
+            initialValues={{
+              firstName: '',
+              secondName: '',
+              email: '',
+              message: '',
+            }}
+            onSubmit={async (values) => {
+              await new Promise((r) => setTimeout(r, 400));
+              sendForm(values);
+            }}
+          >
+            <Form className="FormContact__inside">
+              <label className="FormikContact__label" htmlFor="firstName">Имя</label>
+              <Field
+                className="FormikContact__input"
+                id="firstName"
+                name="firstName"
+                type="firstName"
+              />
 
-            <label className="FormikContact__label" htmlFor="email">Эл. почта *</label>
-            <Field
-              className="FormikContact__input"
-              id="email"
-              name="email"
-              type="email"
-            />
+              <label className="FormikContact__label" htmlFor="secondName">Фамилия</label>
+              <Field
+                className="FormikContact__input"
+                id="secondName"
+                name="secondName"
+                type="secondName"
+              />
 
-            <label className="FormikContact__label" htmlFor="message">Напишите сообщение *</label>
-            <Field
-              component="textarea"
-              rows="3"
-              className="FormikContact__input--large"
-              id="message"
-              name="message"
-              placeholder=""
-              type="message"
-            />
+              <label className="FormikContact__label" htmlFor="email">Эл. почта *</label>
+              <Field
+                className="FormikContact__input"
+                id="email"
+                name="email"
+                type="email"
+              />
 
-            <button className="FormContact__button" type="submit">Отправить</button>
-          </Form>
-        </Formik>
+              <label className="FormikContact__label" htmlFor="message">Напишите сообщение *</label>
+              <Field
+                component="textarea"
+                rows="3"
+                className="FormikContact__input--large"
+                id="message"
+                name="message"
+                placeholder=""
+                type="message"
+              />
+
+              <button className="FormContact__button" type="submit">Отправить</button>
+            </Form>
+          </Formik>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
