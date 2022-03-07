2eb952f980c86e263c990450d20f2885c4923057
Denis Wolter <wolter.important@gmail.com>
2022-03-07 21:23:43 +0300

    mainPage: Add hover(not end), add map function

--- src/features/HomePage/HomePage.js
+++ src/features/HomePage/HomePage.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
 import Header from "../../components/Header";
 import LineWithText from "../../components/LineWithText";
--- src/features/HomePage/components/ShortList/ShortList.js
+++ src/features/HomePage/components/ShortList/ShortList.js
@@ -2,53 +2,46 @@ import React from "react";
 import './ShortList.css';
 
 const ShortList = () => {
+  const servProducts = [ // 12 (id11)
+    {
+      id: 0,
+      name: "Сумка",
+      price: 45,
+      desFirst: "Черная базовая сумка",
+      desSecond: "Кожзам",
+      img: ["22", "22.1"]
+    }, {
+      id: 1,
+      name: "Топ с длинными рукавами",
+      price: 35,
+      desFirst: "Топ в черном и фиолетовом цвете",
+      desSecond: "Размер S,M.",
+      img: ["24", "24.1"]
+    }, {
+      id: 2,
+      name: "Кофта",
+      price: 50,
+      desFirst: "Приятная к телу кофта на замке с принтом.",
+      desSecond: "Размер S,M,L.",
+      img: ["23", "23.1"]
+    },
+
+  ]
+
+  const res = servProducts.map(item =>
+    <div key={item.id} className="list__card">
+      <div className="card_img-container">
+        <img className="card__img--front" src={`img/Catalog/${item.img[0]}.png`} alt=""/>
+        <img className="card__img--behind" src={`img/Catalog/${item.img[1]}.png`} alt=""/>
+      </div>
+      <p className="card__title">{item.name}</p>
+      <p className="card__price">{item.price},00 руб</p>
+    </div>
+  );
+
   return (
     <div className="list">
-      <div className="list__card">
-        <img className="card__img" src="img/Catalog/22.1.png" alt=""/>
-        <p className="card__title">Сумка</p>
-        <p className="card__price">45,00р.</p>
-      </div>
-      <div className="list__card">
-        <img className="card__img" src="img/Catalog/22.1.png" alt=""/>
-        <p className="card__title">Сумка</p>
-        <p className="card__price">45,00р.</p>
-      </div>
-      <div className="list__card">
-        <img className="card__img" src="img/Catalog/22.1.png" alt=""/>
-        <p className="card__title">Сумка</p>
-        <p className="card__price">45,00р.</p>
-      </div>
-      <div className="list__card">
-        <img className="card__img" src="img/Catalog/22.1.png" alt=""/>
-        <p className="card__title">Сумка</p>
-        <p className="card__price">45,00р.</p>
-      </div>
-      <div className="list__card">
-        <img className="card__img" src="img/Catalog/22.1.png" alt=""/>
-        <p className="card__title">Сумка</p>
-        <p className="card__price">45,00р.</p>
-      </div>
-      <div className="list__card">
-        <img className="card__img" src="img/Catalog/22.1.png" alt=""/>
-        <p className="card__title">Сумка</p>
-        <p className="card__price">45,00р.</p>
-      </div>
-      <div className="list__card">
-        <img className="card__img" src="img/Catalog/22.1.png" alt=""/>
-        <p className="card__title">Сумка</p>
-        <p className="card__price">45,00р.</p>
-      </div>
-      <div className="list__card">
-        <img className="card__img" src="img/Catalog/22.1.png" alt=""/>
-        <p className="card__title">Сумка</p>
-        <p className="card__price">45,00р.</p>
-      </div>
-      <div className="list__card">
-        <img className="card__img" src="img/Catalog/22.1.png" alt=""/>
-        <p className="card__title">Сумка</p>
-        <p className="card__price">45,00р.</p>
-      </div>
+      {res}
     </div>
   )
 }
