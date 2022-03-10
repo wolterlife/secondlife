56491c1a95b61be88723790e39bc8a08a6c96d11
Denis Wolter <wolter.important@gmail.com>
2022-03-10 16:48:37 +0300

    Add ItemTopNavFoo

--- src/features/ItemPage/ItemPage.js
+++ src/features/ItemPage/ItemPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ItemNavigation from "./components/ItemNavigation";
 import Header from "../../components/Header";
 import ItemInfo from "./components/ItemInfo";
 import Footer from "../../components/Footer";
@@ -8,7 +7,6 @@ const ItemPage = () => {
   return (
     <>
       <Header />
-      <ItemNavigation />
       <ItemInfo />
       <Footer />
     </>
--- src/features/ItemPage/components/ItemInfo/ItemInfo.js
+++ src/features/ItemPage/components/ItemInfo/ItemInfo.js
@@ -1,12 +1,14 @@
 import React, {useEffect, useState} from "react";
 import styles from './ItemInfo.module.css';
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {database, onValue, ref} from "../../../../util/firebase";
+
 
 const ItemInfo = () => {
   const params = useParams();
+  const navigate = useNavigate();
+  const [counter, setCounter] = useState(1);
   const [dataProducts, setDataProducts] = useState();
-  // eslint-disable-next-line no-unused-vars
   const [isPageLoaded, setLoadStatus] = useState(false);
 
   useEffect(() => { // Получение данных из БД почт+паролей
@@ -17,28 +19,60 @@ const ItemInfo = () => {
   }, []);
 
   function addToCart() {
-    console.log(dataProducts[params.id]);
-
+    const currentItem = Object.assign({quantity: counter}, dataProducts[params.id]);
+    console.log(currentItem);
   }
-
 
   function buyNow() {
-
   }
+
+  function prevItem() { // Навигация. Если пользователь на 0-ом элементе, навигация к последнему.
+    if ((Number(params.id)) === 0) navigate(`/item/${dataProducts.length-1}`)
+    else navigate(`/item/${Number(params.id) - 1}`)
+  }
+
+  function nextItem() { // Навигация. Если пользователь на последнем элементе, навигация к нулевому
+    if ((Number(params.id) + 1) === (dataProducts.length)) navigate('/item/0')
+    else navigate(`/item/${Number(params.id) + 1}`)
+  }
+
+  // Перенести навигационный компонент сюда
 
   return (
     <>
       {isPageLoaded && (
         <div>
+          <div className={styles.containerNav}>
+            <div className={styles.leftContainer}>
+              <button onClick={() => navigate('/')} className={styles.backButton}>Главная&nbsp;</button>
+              <p className={styles.nameOfItem}>/ {dataProducts[params.id].name}</p>
+            </div>
+            <div className={styles.rightContainer}>
+              <div onClick={() => prevItem()} className={styles.leftButton}>
+                <img className={styles.arrowImg} src="/img/left.jpg" alt="Стрелка влево"/>
+                <p className={styles.navLeftText}>Назад</p>
+              </div>
+              <p style={{cursor: "default"}}>|</p>
+              <div onClick={() => nextItem()} className={styles.rightButton}>
+                <p className={styles.navRightText}>Вперёд</p>
+                <img className={styles.arrowImg} src="/img/right.jpg" alt="Стрелка влево"/>
+              </div>
+            </div>
+          </div>
           <div className={styles.container}>
             <img className={styles.imgLarge} src={`/img/Catalog/${dataProducts[params.id].img[0]}.png`} alt=""/>
             <div className={styles.card}>
               <p className={styles.cardName}>{dataProducts[params.id].name}</p>
               <p className={styles.cardPrice}>{dataProducts[params.id].price},00p.</p>
               <p className={styles.card}>Количество</p>
-              <input className={styles.counterInput} defaultValue="1" id="counterInput" name="counterInput"
+              <input className={styles.counterInput}
+                     value={counter}
+                     onChange={e => setCounter(e.target.value)}
+                     id="counterInput"
+                     name="counterInput"
                      type="number"
-                     min="1" max="100"/>
+                     min="1" max="100"
+              />
               <div className={styles.sectionOfButtons}>
                 <button onClick={() => addToCart()} className={styles.buttonAddToCard}>Добавить в корзину</button>
                 <button onClick={() => buyNow()} className={styles.buttonBuyNow}>Купить сейчас</button>
@@ -47,10 +81,10 @@ const ItemInfo = () => {
           </div>
           <div className={styles.description}>
             <p>
-              Приятная к телу кофта на замке с принтом.
+              {dataProducts[params.id].desFirst}
             </p>
             <p>
-              Размер S,M,L
+              {dataProducts[params.id].desSecond}
             </p>
           </div>
         </div>
